0db56d9505e5021a06db02fdb79601e581b7f983
qoo7972365 <32336596+qoo7972365@users.noreply.github.com>
2021-07-30 23:12:10 +0800

    Update change_dns.bat

--- source_code/change_dns.bat
+++ source_code/change_dns.bat
@@ -4,3 +4,5 @@ netsh interface ip set dns name="本地连接" source=static addr=114.114.114.11
 netsh interface ip add dns name="本地连接" addr=114.114.114.115 index=2
 netsh interface ip set dns name="WLAN" source=static addr=114.114.114.114 register=PRIMARY
 netsh interface ip add dns name="WLAN" addr=114.114.114.115 index=2
+netsh interface ip set dns name="已太网" source=static addr=114.114.114.114 register=PRIMARY
+netsh interface ip add dns name="已太网" addr=114.114.114.115 index=2
