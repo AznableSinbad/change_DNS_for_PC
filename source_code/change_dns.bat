13e2c99d9bb7238706a61cbb4c1c860fe0da0f0d
qoo7972365 <32336596+qoo7972365@users.noreply.github.com>
2021-07-31 10:10:37 +0800

    Update change_dns.bat

--- source_code/change_dns.bat
+++ source_code/change_dns.bat
@@ -10,7 +10,8 @@ netsh interface ip set dns name="乙太网" source=static addr=114.114.114.114 r
 netsh interface ip add dns name="乙太网" addr=114.114.114.115 index=2
 netsh interface ip set dns name="Wi-Fi" source=static addr=114.114.114.114 register=PRIMARY
 netsh interface ip add dns name="Wi-Fi" addr=114.114.114.115 index=2
-for /f "tokens=3*" %%t in ('ipconfig ^/all ^| find "以太网适配器"') do set name3=%%t
+
+for /f "tokens=3*" %%t in ('ipconfig ^/all ^| find "以太网"') do set name3=%%t
 
 if "%name3%"=="" ( 
 
@@ -23,4 +24,5 @@ if "%name3%"=="" (
 echo %name%
 netsh interface ip set dns name=%name% source=static addr=114.114.114.114 register=PRIMARY
 netsh interface ip add dns name=%name% addr=114.114.114.115 index=2
+ipconfig/all
 pause
